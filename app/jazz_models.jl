5481e024a0cfe3e98784dd8bc8a758b0c7e4f385
Daniel Harasim <dr.dharasim@gmail.com>
2022-05-16 13:04:41 +0200

    create separate files for model def and eval

--- app/jazz_models.jl
+++ app/jazz_models.jl
@@ -316,33 +316,3 @@ begin # simple product grammar
         (; harmony_rule, ratio)
     end
 end
-
-###################
-# Model execution #
-###################
-
-using ProgressMeter: @showprogress
-
-function naive_supervised_evaluation_accs(grammar, trees)
-    @showprogress map(trees) do tree
-        prediction = predict_tree(grammar, leaflabels(tree))
-        tree_similarity(tree, prediction)
-    end
-end
-
-trees = [tune["harmony_tree"] for tune in treebank];
-grammar = train_on_trees!(simple_harmony_grammar(rulekinds=[:rightheaded]), trees);
-@time mean(naive_supervised_evaluation_accs(grammar, trees))
-
-trees = [tune["rhythm_tree"] for tune in treebank];
-grammar = train_on_trees!(simple_rhythm_grammar(), trees);
-@time mean(naive_supervised_evaluation_accs(grammar, trees))
-
-trees = [tune["harmony_tree"] for tune in treebank];
-grammar = train_on_trees!(transpinv_harmony_grammar(rulekinds=[:rightheaded]), trees);
-mean(naive_supervised_evaluation_accs(grammar, trees))
-
-trees = [tune["product_tree"] for tune in treebank];
-grammar = train_on_trees!(simple_product_grammar(rulekinds=[:rightheaded]), trees);
-@time mean(naive_supervised_evaluation_accs(grammar, trees))
-
